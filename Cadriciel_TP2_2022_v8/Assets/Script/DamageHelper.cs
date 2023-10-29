c80704c98f045f86d2bec34309deaa3cddeeb2bb
LeonardGalibois <60432695+LeonardGalibois@users.noreply.github.com>
2023-10-29 15:12:26 -0400

    Ajout helper de degat pour tester la vie

---
+++ Cadriciel_TP2_2022_v8/Assets/Script/DamageHelper.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageHelper : MonoBehaviour
+{
+    [SerializeField]
+    [Min(1)]
+    private int DamageAmount = 2;
+
+    [SerializeField]
+    [Min(1)]
+    private int HealAmount = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DamageFromClick();
+        HealFromClick();
+    }
+
+    private void DamageFromClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if (Physics.Raycast(mouseRay, out hit))
+            {
+                HealthComponent health = hit.collider.GetComponent<HealthComponent>();
+
+                health?.TakeDamage(DamageAmount);
+            }
+        }
+    }
+
+    private void HealFromClick()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if (Physics.Raycast(mouseRay, out hit))
+            {
+                HealthComponent health = hit.collider.GetComponent<HealthComponent>();
+
+                health?.Heal(HealAmount);
+            }
+
+        }
+    }
+}
