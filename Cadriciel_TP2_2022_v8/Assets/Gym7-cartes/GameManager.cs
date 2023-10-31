241f9de7e161186903c745b9805f59195b90d33c
HugoFoucrit <149535929+HugoFoucrit@users.noreply.github.com>
2023-10-31 16:27:57 -0400

    Add files via upload

---
+++ Cadriciel_TP2_2022_v8/Assets/Gym7-cartes/GameManager.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public UIManager UIM;
+    public GameObject RedKey;
+    public GameObject RedDoor;
+    public GameObject BlueKey;
+    public GameObject BlueDoor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ObtainKey(string Tag){
+        UIM.ObtainKey(Tag);
+        if(Tag=="Red Key"){
+            RedKey.SetActive(false);
+            //RedDoor.SetActive(false);
+        }
+        if(Tag=="Blue Key"){
+            BlueKey.SetActive(false);
+            //BlueDoor.SetActive(false);
+        }
+    }
+
+    public void openDoor(GameObject gameObject)
+    {
+        if (gameObject.tag=="Red Door" && !RedKey.activeInHierarchy) {
+            gameObject.SetActive(false);
+        }
+        
+        if (gameObject.tag == "Blue Door" && !BlueKey.activeInHierarchy)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Cadriciel_TP2_2022_v8/Assets/Gym7-cartes/UI Manager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
+
+public class UIManager : MonoBehaviour
+{
+    public Text UIText;
+    public RawImage RedPanel;
+    public RawImage BluePanel;
+
+    
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ObtainKey(string Tag){
+        if (Tag == "Red Key")
+        {
+            RedPanel.gameObject.SetActive(true);
+
+        }
+        if (Tag == "Blue Key")
+        {
+            BluePanel.gameObject.SetActive(true);
+        }
+    }
+}
